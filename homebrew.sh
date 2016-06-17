e1f5605aa82a2de032daee053bfe104c461d4400
Matt Glick <matt.j.glick@gmail.com>
2016-06-17 00:16:33 -0400

    adding homebrew lists. added comments in bashrc

---
+++ homebrew.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+which -s brew && brew update
+which -s brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+if ! brew doctor; then
+  echo "\`brew doctor\` failed. Please resolve issues before continuing."
+  #exit 1
+fi
+
+brew tap homebrew/binary
+
+formulae=(
+  caskroom/cask/brew-cask
+  caskroom/versions/sublime-text3
+  bash-completion
+  cloc
+  #docker
+  #docker-compose
+  git
+  heroku
+  heroku-toolbelt
+  macvim
+  mysql
+  node
+  nvm
+  phantomjs
+  python
+  slackcat
+  tmux
+  tree
+  wget
+)
+
+casks=(
+  alfred
+  google-chrome
+  iterm2  
+  skype
+  slack
+  teamviewer
+  vlc
+)
+
+for formula in "${formulae[@]}"; do
+  brew install $formula || brew upgrade $formula
+done
+
+for cask in "${casks[@]}"; do
+  brew cask install $cask
+done
