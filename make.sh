4ea03f0e0c73ee8d2229cd48a8ce82d368401261
Matt Glick <matt.j.glick@gmail.com>
2016-06-17 00:26:05 -0400

    cleaned up the makefile

--- make.sh
+++ make.sh
@@ -1,40 +1,29 @@
-#!/bin/bash
+#!/bin/bash -ex
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
+######### HomeBrew Installation
+if [ -a homebrew.sh ]; then
+    ./homebrew.sh
+fi
+
 ########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc gitconfig tmux.conf"    # list of files/folders to symlink in homedir
-
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
 
 # make directories for vim
 mkdir ~/.vim
 mkdir ~/.vim/backup
 mkdir ~/.vim/swap
 mkdir ~/.vim/undo
+mkdir ~/.vim/autoload
 
 # install plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
