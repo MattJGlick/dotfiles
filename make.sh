a3d249aac3ab3d7aa41674c6b8481a95bfb341e7
Matt Glick <matt.j.glick@gmail.com>
2016-06-21 14:27:48 -0400

    Added git origin change and uncommented remaining comments

--- make.sh
+++ make.sh
@@ -5,25 +5,27 @@
 ############################
 
 ######### HomeBrew Installation
-#if [ -a homebrew.sh ]; then
-    #./homebrew.sh
-#fi
+if [ -a homebrew.sh ]; then
+    ./homebrew.sh
+fi
 
 ########## Variables
 files="bashrc vimrc gitconfig tmux.conf"    # list of files/folders to symlink in homedir
 
 # make directories for vim
-#mkdir ~/.vim
-#mkdir ~/.vim/backup
-#mkdir ~/.vim/swap
-#mkdir ~/.vim/undo
-#mkdir ~/.vim/autoload
+mkdir ~/.vim
+mkdir ~/.vim/backup
+mkdir ~/.vim/swap
+mkdir ~/.vim/undo
+mkdir ~/.vim/autoload
 
 # install plugged
-#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # create symlinks
 for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s ~/dotfiles/$file ~/.$file
 done
+
+git remote set-url origin git@github.com:MattJGlick/dotfiles.git
